19d7f4cb61135526fcdea7b032ca0b2a9d3ad147
Jack Tomlinson <tomlinson.johnq@gmail.com>
2025-10-22 16:55:12 -0500

    imporved coding techniques

--- meeting_3/useful_example.py
+++ meeting_3/useful_example.py
@@ -1,7 +1,7 @@
 password = input("Enter the password: ")
 num_of_attempts = 1
 
-while password != "secret":
+while (password != "secret"):
     password = input("Enter the password: ")
     num_of_attempts += 1
 

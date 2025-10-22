ba751b2ab6f7483644b61026ee2ab42922f2ffda
Jack Tomlinson <tomlinson.johnq@gmail.com>
2025-10-22 17:05:34 -0500

    changed car names and added the harder activity

--- activity_solution.py
+++ activity_solution.py
@@ -1,14 +1,14 @@
 import random as rand
 
-target_number = rand.randint(1, 100)
+random_number = rand.randint(1, 100)
 guess = None
 num_attempts = 0
-while guess != target_number:
+while guess != random_number:
     guess = int(input("Guess a number between 1 and 100: "))
-    if guess < target_number:
+    if guess < random_number:
         print("Too low! ")
-    elif guess > target_number:
+    elif guess > random_number:
         print("Too high! ")
     num_attempts += 1
 
-print(f"Congratulations! You've guessed the number {target_number} in {num_attempts} attempts.")
+print(f"Congratulations! You've guessed the number {random_number} in {num_attempts} attempts.")

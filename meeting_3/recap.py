4c55e11d1c94470303c50abee79ca1217f70d927
Jack Tomlinson <tomlinson.johnq@gmail.com>
2025-10-22 16:41:48 -0500

    recap activity going over loops

--- meeting_3/recap.py
+++ meeting_3/recap.py
@@ -1,1 +1,10 @@
-print("hello world")
+cost_of_sandwich = 5
+
+answer = int(input("How much money do you have?"))
+
+if answer == 1000:
+    print("You have exactly 1000 dollars!")
+elif answer >= cost_of_sandwich:
+    print("You can buy a sandwich!")
+else:
+    print("You can't buy anything.")
